4bb51ff453ec3d0eef4adaa8d951b832c6be0d1d
josafams <devjosafa@gmail.com>
2019-01-28 20:22:00 -0300

    add tipos de login

--- login/conn.php
+++ login/conn.php
@@ -1,13 +1,12 @@
 <?php
-	$db_name = "base0";
+	
+	echo "aqui";
+
+	$db_name = "mkradius";
 	$mysql_username = "root";
 	$mysql_password = "vertrigo";
-	$server_name = "localhost";
+	$server_name = "192.168.1.240:3306";
 	$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
-	
-	if($conn){
-		echo "sucesso";
-	}else {
-		echo "falha";
-	}
+
+
 ?>
--- login/login.php
+++ login/login.php
@@ -1,17 +1,53 @@
 <?php
+	echo "aqui";
 	require "conn.php";
 
-	$user_name = "j@email.com";
-	$user_pass = "123456";
+	//header('Content-Type: application/json; charset=utf-8');
 
-	$mysql_qery = "select * from login where email like '$user_name' and senha like '$user_pass' ";
+	$user_name = $_POST["user_name"];
+	$user_pass = $_POST["user_password"];
 
-	$result = mysqli_query($conn, $mysql_qery);
+
+	$query = "select * from sis_central";
+	//$query = "select * from sis_cliente where login like '$user_name' and senha like '$user_pass' ";
+
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_assoc($result);
+
+
+	switch ($row["fsenha"]) {
+	 	case 'senha':
+	 		// login cpf
+	 		$query = "select * from sis_cliente where login like '$user_name' and senha like '$user_pass' ";
+	 		break;
+	 	case 'lcpf'
+	 		// login senha
+	 		$query = "select * from sis_cliente where login like '$user_name' and cpf_cnpj like '$user_pass' ";
+	 		break;
+	 	case 'cpfs':
+	 		// login cpf
+	 		$query = "select * from sis_cliente where cpf_cnpj like '$user_name' and senha like '$user_pass' ";
+	 		break;
+	 	case 'cpf'
+	 		// cpf 
+	 		$query = "select * from sis_cliente where cpf_cnpj like '$user_name' ";
+	 		break;
+	 	default:
+	 		# code...
+	 		break;
+	 };
+
+	$result = mysqli_query($conn, $query);
+
+	echo $result;
+	echo $query;
 
 	if(mysqli_num_rows($result) > 0){
-		echo "<br> login sucesso";
+		$response =  array('error' => 0 );
+		echo json_encode($response);  
 	} else {
-		echo "<br> login falho";
+		$response =  array('error' => 1);
+		echo json_encode($response); 
 	}
 ?>
 
