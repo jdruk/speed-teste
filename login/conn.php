7407a310b937cb8f507f08190af1064bc9e09a49
jdruk <devjosafa@gmail.com>
2019-01-30 22:33:42 -0200

    alterado informações dentro de index

--- login/conn.php
+++ login/conn.php
@@ -1,7 +1,4 @@
 <?php
-	
-	echo "aqui";
-
 	$db_name = "mkradius";
 	$mysql_username = "root";
 	$mysql_password = "vertrigo";
@@ -9,4 +6,11 @@
 	$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
 
 
+	define("TOKEN", 'a0910677-dd0a-4bf1-84ef-bc9a8f148a8c');
+	define('TOKEN_ERROR', '666');
+	
+
+	define('SUCESS', 0);
+	define('LOGIN_NOT_INFORMED', 1);
+	define('UNEXISTENT_USER', 2);
 ?>
--- login/login.php
+++ login/login.php
@@ -1,26 +1,28 @@
 <?php
-	echo "aqui";
+
 	require "conn.php";
 
 	//header('Content-Type: application/json; charset=utf-8');
 
-	$user_name = $_POST["user_name"];
-	$user_pass = $_POST["user_password"];
+	function check_type_login(){
+		global $conn;
 
+		$query = "select * from sis_central";
+	
+		$result = mysqli_query($conn, $query);
+		$row = mysqli_fetch_assoc($result);
 
-	$query = "select * from sis_central";
-	//$query = "select * from sis_cliente where login like '$user_name' and senha like '$user_pass' ";
+		return $row["fsenha"];
+	}
 
-	$result = mysqli_query($conn, $query);
-	$row = mysqli_fetch_assoc($result);
-
-
-	switch ($row["fsenha"]) {
+	function select_query($type_login, $user_name, $user_pass){
+		$query = "";
+		switch ($type_login) {
 	 	case 'senha':
 	 		// login cpf
 	 		$query = "select * from sis_cliente where login like '$user_name' and senha like '$user_pass' ";
 	 		break;
-	 	case 'lcpf'
+	 	case 'lcpf':
 	 		// login senha
 	 		$query = "select * from sis_cliente where login like '$user_name' and cpf_cnpj like '$user_pass' ";
 	 		break;
@@ -28,26 +30,62 @@
 	 		// login cpf
 	 		$query = "select * from sis_cliente where cpf_cnpj like '$user_name' and senha like '$user_pass' ";
 	 		break;
-	 	case 'cpf'
+	 	case 'cpf':
 	 		// cpf 
 	 		$query = "select * from sis_cliente where cpf_cnpj like '$user_name' ";
 	 		break;
-	 	default:
-	 		# code...
-	 		break;
-	 };
+	 	};
 
-	$result = mysqli_query($conn, $query);
-
-	echo $result;
-	echo $query;
-
-	if(mysqli_num_rows($result) > 0){
-		$response =  array('error' => 0 );
-		echo json_encode($response);  
-	} else {
-		$response =  array('error' => 1);
-		echo json_encode($response); 
+	 	return $query;
 	}
+
+	function response_user($result){
+		if($result == null || mysqli_num_rows($result) <= 0){
+			//return array('error' => UNEXISTENT_USER );
+			return "falha";
+		}
+
+		//$row = mysqli_fetch_assoc($result);
+		
+		//$response -> error = SUCESS;
+		//$response -> name = $row['nome'];
+		return 'sucesso';
+		//return $response;
+	}
+
+	function response_for($user_name, $user_password){
+		$query = select_query(check_type_login(),$user_name, $user_password);
+		$result = execute_query($query);
+
+		//echo json_encode(response_user($result));
+		echo (response_user($result));
+	}
+
+	function execute_query($query){
+		global $conn;
+		if($query == ""){
+			return null;
+		}
+		return mysqli_query($conn, $query);
+	}
+
+	function init_components(){
+		if(isset($_POST["user_name"]) && isset($_POST["user_password"])){
+			$user_name = $_POST["user_name"];
+			$user_password = $_POST["user_password"];
+
+			response_for($user_name, $user_password);
+
+		} else {
+			echo json_encode(array('error' => LOGIN_NOT_INFORMED));
+		}
+	}
+	
+
+	function main(){
+		init_components();
+	}
+	
+	main();
 ?>
 
